875148aaed1bcb211d211f28b53a3f197c9d078c
Ben Scholar <ben@benscholar.com>
2024-04-23 21:01:49 -0700

    attempt to fix opencv gstreamer

--- install_opi5.sh
+++ install_opi5.sh
@@ -37,7 +37,7 @@ netplan apply
 
 # Install necessary packages
 echo "Installing packages"
-apt-get install -y wget build-essential cmake libffi-dev libssl-dev zlib1g-dev curl libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libtbbmalloc2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-dev gfortran openexr libatlas-base-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base libgstreamer-plugins-bad1.0-dev  gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-gl clang python3 python3-pip
+apt-get install -y wget build-essential cmake libffi-dev libssl-dev zlib1g-dev curl libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libtbbmalloc2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-dev gfortran openexr libatlas-base-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base libgstreamer-plugins-bad1.0-dev  gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-gl clang python3 python3-pip gstreamer1.0-opencv libgstreamer1.0-0 libgstreamer1.0-dev
 
 # install python deps
 pip3 install -v numpy
@@ -48,7 +48,7 @@ pip3 install -v pillow
 
 git clone --recursive https://github.com/opencv/opencv-python.git
 cd opencv-python
-export CMAKE_ARGS="-DWITH_GSTREAMER=ON"
+export CMAKE_ARGS="-DWITH_GSTREAMER=ON -DWITH_FFMPEG=OFF"
 export ENABLE_CONTRIB=1
 export ENABLE_HEADLESS=1
 pip3 wheel . --verbose
