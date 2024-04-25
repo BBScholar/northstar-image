0e97a05d4796a3b68c2f58cc3c26a555bec7b45f
Ben Scholar <ben@benscholar.com>
2024-04-25 16:26:59 -0700

    attempt to fix

--- install_opi5.sh
+++ install_opi5.sh
@@ -37,9 +37,9 @@ netplan apply
 
 # Install necessary packages
 echo "Installing packages"
-apt-get install -y wget build-essential cmake libffi-dev libssl-dev zlib1g-dev curl libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libtbbmalloc2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-dev gfortran openexr libatlas-base-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base libgstreamer-plugins-bad1.0-dev  gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-gl clang python3 python3-pip gstreamer1.0-opencv libgstreamer1.0-0 libgstreamer1.0-dev
+apt-get install --no-install-recommends -y wget build-essential cmake libffi-dev libssl-dev zlib1g-dev curl libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libtbbmalloc2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-dev gfortran openexr libatlas-base-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base libgstreamer-plugins-bad1.0-dev  gstreamer1.0-plugins-bad gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-gl clang python3 python3-pip gstreamer1.0-opencv libgstreamer1.0-0 libgstreamer1.0-dev
 
-apt-get install -y \
+apt-get install --no-install-recommends -y \
   gstreamer1.0-gl \
   gstreamer1.0-opencv \
   gstreamer1.0-video \
@@ -52,12 +52,14 @@ apt-get install -y \
   libgstreamer1.0-dev
 
 git clone --depth=1 https://github.com/airockchip/librga.git
-cd librga/include
-sudo cp ./*.h /usr/local/include
-cd ..
-cd librga/libs/Linux/gcc-aarch64
-sudo cp ./lib* /usr/local/lib
-cd ../../../..
+
+cp -r librga/include/*.h /usr/local/include
+cp -r librga/libs/Linux/gcc-aarch64/lib* /usr/local/lib
+# cp ./*.h /usr/local/include
+# cd ..
+# cd libs/Linux/gcc-aarch64
+# sudo cp ./lib* /usr/local/lib
+# cd ../../../..
  
 # apt-get install -y gstreamer1.0*
 # install python deps
